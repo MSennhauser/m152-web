40306aab884ccc896c19350d8a92d0e0074e7b66
Michael <sennhauser.michael@gmail.com>
2021-12-17 07:49:47 +0100

    router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './features/home/home.component';
+
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+  scrollOffset: [0, 64],
+};
 
 const routes: Routes = [
   {
@@ -10,12 +17,12 @@ const routes: Routes = [
   }, {
     path: 'home',
     pathMatch: 'full',
-    component: HomeComponent
+    component: AppComponent,
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
